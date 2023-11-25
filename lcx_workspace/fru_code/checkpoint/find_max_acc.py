67a97f94d12883c6844b19571626a8b5a08f6f3e
Axboexx <58794211+Axboexx@users.noreply.github.com>
2023-11-25 10:26:25 +0800

    Add files via upload

--- lcx_workspace/fru_code/checkpoint/find_max_acc.py
+++ lcx_workspace/fru_code/checkpoint/find_max_acc.py
@@ -1,25 +1,43 @@
 import time
 import argparse
 
-parser = argparse.ArgumentParser(description='set parama')
-parser.add_argument('--path', default='', type=str, help="path of log.txt")
-args = parser.parse_args()
-
-file = './' + args.path + '/log.txt'
-f = open(file, 'r')
-line = f.readline()
-max_1 = 0
-max_5 = 0
-epoch = 0
-max_epoch = 0
-while line != '':
-    words = line.split(' ')
-    top1 = float(words[2][:6])
-    top5 = float(words[4][:6])
-    epoch = epoch + 1
-    if top1 > max_1:
-        max_1 = top1
-        max_5 = top5
-        max_epoch = epoch
+def find_max(file):
+    f = open(file, 'r')
     line = f.readline()
-print(max_1, max_5, max_epoch)
+    max_1 = 0
+    epoch = 0
+    while line != '':
+        words = line.split(' ')
+        top1 = float(words[2][:-1])
+        top5 = float(words[4][:-1])
+        epoch = epoch + 1
+        if top1 > max_1:
+            max_1 = top1
+        line = f.readline()
+    return max_1
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='set parama')
+    parser.add_argument('--path', default='', type=str, help="path of log.txt")
+    args = parser.parse_args()
+
+    file = './' + args.path + '/log.txt'
+    f = open(file, 'r')
+    line = f.readline()
+    max_1 = 0
+    max_5 = 0
+    epoch = 0
+    max_epoch = 0
+    while line != '':
+        words = line.split(' ')
+        top1 = float(words[2][:-1])
+        top5 = float(words[4][:-1])
+        epoch = epoch + 1
+        if top1 > max_1:
+            max_1 = top1
+            max_5 = top5
+            max_epoch = epoch
+        line = f.readline()
+    print(max_1, max_5, max_epoch)
